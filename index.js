4c2dc109435cb925d449da2de38332731cf04f01
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-03-29 22:50:41 -0400

    style side nav and create dropdown function

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ ClassicEditor
             .catch( error => {
                 console.error( error )
             } )
+
+const predefined = document.getElementById('predefined')
+const custom = document.getElementById('custom')
+const framework = document.getElementById('framework')
+const treeItemNestedList = document.querySelectorAll('.tree-item-nested-list')
+
+
+predefined.addEventListener('click', function(){
+    treeItemNestedList[0].classList.toggle('d-none')
+})
+
+custom.addEventListener('click', function(){
+    treeItemNestedList[1].classList.toggle('d-none')
+})
+
+framework.addEventListener('click', function(){
+    treeItemNestedList[2].classList.toggle('d-none')
+})
