ee37b99de1b0a0d0aa970493e24bf63602f85aa0
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-03-31 12:48:45 -0400

    investigate on adding hotjar feedback

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const predefined = document.getElementById('predefined')
 const custom = document.getElementById('custom')
 const framework = document.getElementById('framework')
 const treeItemNestedList = document.querySelectorAll('.tree-item-nested-list')
+const feedbackBtn = document.getElementById('feedback-btn')
 
 
 predefined.addEventListener('click', function(){
@@ -22,23 +23,3 @@ framework.addEventListener('click', function(){
     treeItemNestedList[2].classList.toggle('d-none')
 })
 
-const handleSearch = function(version) {
-    if ( version === 'online' ) {
-        this.$( 'head' ).append( '<link rel="stylesheet" href="https://cdn.jsdelivr.net/docsearch.js/2/docsearch.min.css">' );
-        this.$( 'body' ).append( '<script type="text/javascript" ' +
-            'src="https://cdn.jsdelivr.net/docsearch.js/2/docsearch.min.js"></script>' );
-        this.$( 'body' ).append( '<script type="text/javascript"> docsearch({' +
-            'apiKey: "43f8f34ee5b472d5fe0cdca7777d776b",' +
-            'indexName: "ckeditor",' +
-            'inputSelector: "#docsearch_input",' +
-            'debug: false' +
-            '});' +
-            '</script>' );
-    } else {
-        this.$( '.search-container' ).css( 'display', 'none' );
-    }
-}
-
-sample.handleSearch( opts.version )
-index.handleSearch( opts.version )
-license.handleSearch( opts.version )
